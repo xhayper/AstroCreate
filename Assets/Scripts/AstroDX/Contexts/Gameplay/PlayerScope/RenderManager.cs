7b9b330567cef3559d3301bbfee8823d0bef965c
xhayper <hayper1919@gmail.com>
2023-10-07 23:33:06 +0700

    feat: add AstroDX code

---
+++ Assets/Scripts/AstroDX/Contexts/Gameplay/PlayerScope/RenderManager.cs
@@ -0,0 +1,12 @@
+namespace AstroDX.Contexts.Gameplay.PlayerScope
+{
+    public static class RenderManager
+    {
+        public const float PlayFieldRadius = 4.33f;
+        public const float AreaARadius = 3.6f;
+        public const float AreaBRadius = 2f;
+        public const float AreaDRadius = 3.8f;
+        public const float AreaERadius = 2.8114f;
+        public const float CenterRadius = 1.7f;
+    }
+}
